52049b14a6d02756bb9b38880dcbaa4b946ea1c7
pikapower9080 <pikapower9080@users.noreply.github.com>
2023-05-09 08:56:28 -0400

    Still kinda broken but progress

--- index.js
+++ index.js
@@ -11,57 +11,57 @@
 
 let qm = {}
 
-qm.css = `#quick-menu-overlay {
-            position: fixed;
-            z-index: 1000;
-            background-color: rgba(0, 0, 0, 0.5);
-            width: 100vw;
-            height: 100vh;
-            top: 0;
-            left: 0;
-        }
-        #quick-menu {
-            z-index: 1001;
-            position: fixed;
-            top: 20%;
-            left: 50%;
-            transform: translateX(-50%);
-            width: 300px;
-            font-family: monospace;
-        }
-        #quick-menu input {
-            width: 100%;
-            font-size: 16px;
-            font-family: monospace;
-            border-radius: 0px;
-            border-top-left-radius: 5px;
-            border-top-right-radius: 5px;
-            border: 1px solid black;
-            padding: 4px;
-            margin: 0;
-        }
-        #quick-menu input:focus {
-            outline: none;
-        }
-        #quick-menu #results {
-            background-color: white;
-            border: 1px solid black;
-            border-top: none;
-            border-bottom: none;
-            width: calc(300px + 8px);
-        }
-        #quick-menu .result {
-            padding: 3px;
-            border-bottom: 1px solid black;
-        }`
+qm.css = `
+#quick-menu-overlay {
+    position: fixed;
+    z-index: 1000;
+    background-color: rgba(0, 0, 0, 0.5);
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+}
+#quick-menu {
+    z-index: 1001;
+    position: fixed;
+    top: 20%;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 300px;
+    font-family: monospace;
+}
+#quick-menu input {
+    width: 100%;
+    font-size: 16px;
+    font-family: monospace;
+    border-radius: 0px;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    border: 1px solid black;
+    padding: 4px;
+    margin: 0;
+}
+#quick-menu input:focus {
+    outline: none;
+}
+#quick-menu #results {
+    background-color: white;
+    border: 1px solid black;
+    border-top: none;
+    border-bottom: none;
+    width: calc(300px + 8px);
+}
+#quick-menu .result {
+    padding: 3px;
+    border-bottom: 1px solid black;
+}
+`
 
 qm.html = `
     <div id="quick-menu-overlay"></div>
     <div id="quick-menu">
-        <form id="quick-menu-form">
-            <input type="text" id="quick-menu-input" placeholder="Search Anything...">
-            <input type="submit" style="display: none;">
-        </form>
+        <input type="text" id="quick-menu-input" placeholder="Search Anything...">
+        <input type="submit" style="display: none;">
         <div id="results">
             <template class="result" id="result-template">
                 <div class="result">
@@ -94,24 +94,54 @@ qm.fuseScript.src = "https://cdn.jsdelivr.net/npm/fuse.js@6.6.2"
 document.head.appendChild(qm.fuseScript)
 
 function setupKeybind() {
+    qm.fuse = new Fuse(qm.items, {
+        keys: ["title", "name"]
+    })
     addEventListener("keydown", (e) => {
         if ((e.key == "P" && e.ctrlKey) || (e.key == "Escape" && qm.container.style.display == "none")) {
+            e.preventDefault()
             qm.container.style.display = "unset"
             qm.input.focus()
+            qm.input.value = ""
         }
         else if (e.key == "Escape" && qm.container.style.display == "unset") {
             qm.input.blur()
         }
     })
+    qm.input.addEventListener("input", () => {
+        qm.results.querySelectorAll(".result").forEach(r => r.remove())
+        qm.fuse.search(qm.input.value).forEach((item) => {
+            const clone = qm.resultTemplate.content.cloneNode(true)
+            clone.querySelector(".mainText").innerText = item.item.name || item.item.title
+            qm.results.appendChild(clone)
+        })
+    })
 }
 
-fetch("https://hcpss.instructure.com/api/v1/users/self/favorites/courses", {}).then((res) => {
+
+qm.items = []
+
+fetch("https://hcpss.instructure.com/api/v1/users/self/favorites/courses?include[]=banner_image", {}).then((res) => {
     res.json().then((json) => {
         json.forEach((course) => {
-            const clone = qm.resultTemplate.content.cloneNode(true)
-            clone.querySelector(".mainText").innerText = course.name
-            qm.results.appendChild(clone);
+            qm.items.push(course)
         })
-        setupKeybind();
+        setupKeybind()
     })
 })
+/* 
+{
+    "id": 12117972,
+    "title": "U6L4- Board Events",
+    "position": 3,
+    "indent": 2,
+    "quiz_lti": false,
+    "type": "Assignment",
+    "module_id": 1360071,
+    "html_url": "https://hcpss.instructure.com/courses/185913/modules/items/12117972",
+    "content_id": 8827977,
+    "url": "https://hcpss.instructure.com/api/v1/courses/185913/assignments/8827977",
+    "course_id": 185913,
+    "course_name": "Computer Science"
+}
+*/
