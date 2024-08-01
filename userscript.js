c28386386d2ab735a8944b539c42419e0e56b40b
pika <pikapower9080@gmail.com>
2024-08-01 14:18:10 -0400

    Update userscript to avoid CORS errors

--- userscript.js
+++ userscript.js
@@ -6,7 +6,7 @@
 // @author       pikapower9080
 // @match        https://*.instructure.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=canvas.instructure.com
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // @noframes
 // ==/UserScript==
 
@@ -15,13 +15,27 @@ let cache = sessionStorage.getItem("qm-cache")
 
 if (!cache) {
     console.log("No cache found, loading quick menu from url: " + fetchUrl)
-    fetch(fetchUrl).then((res) => {
-        res.text().then((code) => {
-            sessionStorage.setItem("qm-cache", code)
-            console.log("Canvas quick menu is now cached in session storage")
-            new Function(code)()
+    if (GM_xmlhttpRequest) {
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: fetchUrl,
+            onload: function(response) {
+                const code = response.responseText
+                sessionStorage.setItem("qm-cache", code)
+                console.log("Canvas quick menu is now cached in session storage")
+                new Function(code)()
+            }
+        });
+    } else {
+        console.warn("Canvas Quick Menu couldn't use the GM_xmlhttpRequest method, falling back to fetch. Exepct CORS errors.")
+        fetch(fetchUrl).then((res) => {
+            res.text().then((code) => {
+                sessionStorage.setItem("qm-cache", code)
+                console.log("Canvas quick menu is now cached in session storage")
+                new Function(code)()
+            })
         })
-    })
+    }
 } else {
     console.log("Loading quick menu from session cache")
     new Function(cache)()
