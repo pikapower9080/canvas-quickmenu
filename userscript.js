a616515bc74bb322e003b9f1eda14559a69a6ee1
Ben <pikapower9080@gmail.com>
2024-09-02 19:51:55 -0400

    Add @connect metadata to userscript to prevent cross site request popup

--- userscript.js
+++ userscript.js
@@ -7,6 +7,7 @@
 // @match        https://*.instructure.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=canvas.instructure.com
 // @grant        GM_xmlhttpRequest
+// @connect      gist.githubusercontent.com
 // @noframes
 // ==/UserScript==
 
